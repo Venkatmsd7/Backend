1eadc7c064d85c6afd58089c5ed4bbda63f1d6d0
Venkatmsd7 <sastrymsd77@gmail.com>
2023-11-24 21:14:49 +0530

    added clodinary util and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2} from "cloudinary";
+import fs from "fs";
+
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilepath) => {
+    try {
+        
+        if (!localFilepath) return null
+        await cloudinary.uploader.upload(localFilepath,{
+            resource_type:"auto"
+        })
+        console.log("file has been uploaded")
+    } catch (error) {
+        
+        fs.unlinkSync(localFilepath) //remove the file as the upload failed
+    }
+} 
+
+export default uploadOnCloudinary
