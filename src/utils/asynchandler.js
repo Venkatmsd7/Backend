99d49276c527d64c931b86a01d8565e2a95adc6f
Venkatmsd7 <sastrymsd77@gmail.com>
2023-11-26 15:33:55 +0530

    added register in user controllers

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,7 +1,7 @@
-const asynsHandler = (requestHandler) =>{
-    (req,res,next) =>{
-        Promise.resolve(requestHandler(res,req,next)).catch((err)=>next(err))
+const asyncHandler = (requestHandler) =>{
+    return (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
 
-export {asynsHandler}
+export {asyncHandler}
